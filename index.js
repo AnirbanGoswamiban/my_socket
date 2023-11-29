05afcf38e85396089b9c2f0405feeb2999b49fc2
anirban <anirbangoswami323@gmail.com>
2023-11-29 10:48:16 +0530

    s commit

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-const { log } = require("console");
-require('dotenv').config();
-const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const httpServer = createServer();
+const io = new Server(httpServer);
 const port = process.env.PORT || 3000;
 
-let total_client=new Set()
+let total_clients = new Set();
 
-io.on("connection",(socket)=>{
-    console.log("connected",socket.id);
-    total_client.add(socket.id)
-    io.emit("client-joined",socket.id)
-    io.emit("totalclients",total_client.size)
-    socket.on("message",(data)=>{
-        console.log(data);
-    socket.broadcast.emit("chat-message",data)
-    })
-    socket.on("disconnect",()=>{
-        console.log(socket.id)
-        total_client.delete(socket.id)
-        io.emit("client-left",socket.id)
-        io.emit("totalclients",total_client.size)
+io.on("connection", (socket) => {
+  console.log("Connected", socket.id);
+  total_clients.add(socket.id);
+  io.emit("client-joined", socket.id);
+  io.emit("total-clients", total_clients.size);
 
-    })
-})
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("chat-message", data);
+  });
 
-httpServer.listen(port,()=>{
-    console.log(`server is runnin ${port}`);
+  socket.on("disconnect", () => {
+    console.log(socket.id);
+    total_clients.delete(socket.id);
+    io.emit("client-left", socket.id);
+    io.emit("total-clients", total_clients.size);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Socket server is running on port ${port}`);
 });
